0979efec656eca97eab887facfe106aacfaee432
hytdsh <hytdsh@gmail.com>
2019-10-18 11:15:48 +0900

    14.2.2 演習

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -14,9 +14,19 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
+    assert_select 'strong#following', @user.following.count.to_s
+    assert_select 'strong#followers', @user.followers.count.to_s
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+  end
+
+  test "home page profile" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'strong#following', @user.following.count.to_s
+    assert_select 'strong#followers', @user.followers.count.to_s
   end
 end
